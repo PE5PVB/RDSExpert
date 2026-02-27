f39cd7c5d1596c5e53f2b77f2d1a990d23106855
Lucas Gallone <lucasgallone@outlook.fr>
2026-02-27 02:28:32 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -789,7 +789,7 @@ const App: React.FC = () => {
 
     // 8. GROUPS COUNTER
     const G_DESC: Record<string, string> = {
-      "0A": "PI, PS, AF, PTY, Flags", "0B": "PI, PS, PTY, Flags", "1A": "ECC, LIC, PIN, EWS ID", "1B": "ECC, LIC, PIN, EWS ID",
+      "0A": "PI, PS, AF, PTY, Flags", "0B": "PI, PS, PTY, Flags", "1A": "ECC, LIC, PIN, EWS ID", "1B": "PIN",
       "2A": "Radiotext", "2B": "Radiotext", "3A": "ODA AIDs List", "3B": "ODA",
       "4A": "CT - Time & Date", "4B": "ODA", "5A": "TDC / ODA", "5B": "TDC / ODA",
       "6A": "In House Applications / ODA", "6B": "In House Applications / ODA", "7A": "Radio Paging / ODA", "7B": "Radio Paging / ODA",
